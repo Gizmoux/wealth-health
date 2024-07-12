4913034356004b937c9317c29ab5763b45f05e08
Nicolas <nicolas.deriemacker@hotmail.fr>
2024-07-12 16:12:16 +0200

    Add page employee

--- app/pages/employee-list/page.tsx
+++ app/pages/employee-list/page.tsx
@@ -1,8 +1,11 @@
 'use client';
+import { Input } from '@/components/ui/input';
 import React, { useEffect, useState } from 'react';
 import Link from 'next/link';
 import DataTable from 'react-data-table-component';
 import 'tailwindcss/tailwind.css';
+import { ArrowUpDown } from 'lucide-react';
+import PaginationComp from '@/app/components/PaginationComp';
 
 const EmployeeList: React.FC = () => {
 	const [employees, setEmployees] = useState([]);
@@ -28,8 +31,29 @@ const EmployeeList: React.FC = () => {
 
 	return (
 		<div className="container mx-auto p-4">
-			<h1>Current Employees</h1>
+			<h1 className="bg-primary rounded-md w-48 p-2 m-2 hover:bg-primary-foreground">
+				Current Employees
+			</h1>
+			<div className="flex justify-between items-center">
+				<h2>
+					Show{'  '}
+					<select>
+						<option>10</option>
+						<option>25</option>
+						<option>50</option>
+						<option>100</option>
+					</select>
+					entries
+				</h2>
+				<div className="flex items-center gap-2">
+					<label htmlFor="">Search</label>
+					<Input />
+				</div>
+			</div>
 			<DataTable columns={columns} data={employees} />
+			<ArrowUpDown className="ml-2 h-4 w-4" />
+
+			<PaginationComp />
 			<Link href="/">Home</Link>
 		</div>
 	);
