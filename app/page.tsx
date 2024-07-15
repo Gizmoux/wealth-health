bd49c605c3217bb773ba07fdf21df951ee971dd2
Nicolas <nicolas.deriemacker@hotmail.fr>
2024-07-15 14:13:55 +0200

    Add home page

--- app/page.tsx
+++ app/page.tsx
@@ -7,13 +7,21 @@ const Home = () => {
 	return (
 		<div className="">
 			<Header />
-			<div className="flex justify-center items-center">
-				<Image
-					src={healthImage}
-					width={500}
-					height={500}
-					alt="healthiswealth"
-				/>
+			<div className="flex flex-col justify-center items-center p-10">
+				<h1>Notre projet</h1>
+
+				<p className="w-1/2 h-1/2 bg-green-100 rounded-xl p-4">
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam
+					itaque possimus aperiam tempora suscipit quia ad dolorem dolor, odio
+					laboriosam, corporis facere. Aliquid, doloremque veniam ea quidem
+					nulla in minima? Lorem ipsum dolor sit amet consectetur adipisicing
+					elit. Explicabo nulla veritatis corporis laudantium officiis iste quae
+					voluptatem? Dolorum, quo illum? Ratione explicabo cum rem sapiente
+					labore possimus aperiam dolor perspiciatis? Lorem ipsum dolor sit amet
+					consectetur adipisicing elit. Eos ipsum accusantium corporis similique
+					nam ipsam quidem dignissimos culpa unde, ea, illo molestiae quaerat
+					explicabo libero optio doloremque veniam veritatis voluptatem.
+				</p>
 			</div>
 		</div>
 	);
