52e94398773680e6336a0204ddcbb33e6d22db93
Nicolas <nicolas.deriemacker@hotmail.fr>
2024-07-15 14:14:21 +0200

    change emlployeelist component

--- app/pages/employee-list/page.tsx
+++ app/pages/employee-list/page.tsx
@@ -1,11 +1,9 @@
 'use client';
 import { Input } from '@/components/ui/input';
 import React, { useEffect, useState } from 'react';
-import Link from 'next/link';
 import DataTable from 'react-data-table-component';
 import 'tailwindcss/tailwind.css';
-import { ArrowUpDown } from 'lucide-react';
-import PaginationComp from '@/app/components/PaginationComp';
+import Header from '@/app/components/Header';
 
 const EmployeeList: React.FC = () => {
 	const [employees, setEmployees] = useState([]);
@@ -18,19 +16,36 @@ const EmployeeList: React.FC = () => {
 	}, []);
 
 	const columns = [
-		{ name: 'First Name', selector: (row: any) => row.firstName },
-		{ name: 'Last Name', selector: (row: any) => row.lastName },
-		{ name: 'Start Date', selector: (row: any) => row.startDate },
-		{ name: 'Department', selector: (row: any) => row.department },
-		{ name: 'Date of Birth', selector: (row: any) => row.dateOfBirth },
-		{ name: 'Street', selector: (row: any) => row.street },
-		{ name: 'City', selector: (row: any) => row.city },
-		{ name: 'State', selector: (row: any) => row.state },
-		{ name: 'Zip Code', selector: (row: any) => row.zipCode },
+		{
+			name: 'First Name',
+			selector: (row: any) => row.firstName,
+			sortable: true,
+		},
+		{ name: 'Last Name', selector: (row: any) => row.lastName, sortable: true },
+		{
+			name: 'Start Date',
+			selector: (row: any) => row.startDate,
+			sortable: true,
+		},
+		{
+			name: 'Department',
+			selector: (row: any) => row.department,
+			sortable: true,
+		},
+		{
+			name: 'Date of Birth',
+			selector: (row: any) => row.dateOfBirth,
+			sortable: true,
+		},
+		{ name: 'Street', selector: (row: any) => row.street, sortable: true },
+		{ name: 'City', selector: (row: any) => row.city, sortable: true },
+		{ name: 'State', selector: (row: any) => row.state, sortable: true },
+		{ name: 'Zip Code', selector: (row: any) => row.zipCode, sortable: true },
 	];
 
 	return (
-		<div className="container mx-auto p-4">
+		<div className="mx-auto p-2">
+			<Header />
 			<h1 className="bg-primary rounded-md w-48 p-2 m-2 hover:bg-primary-foreground">
 				Current Employees
 			</h1>
@@ -50,11 +65,7 @@ const EmployeeList: React.FC = () => {
 					<Input />
 				</div>
 			</div>
-			<DataTable columns={columns} data={employees} />
-			<ArrowUpDown className="ml-2 h-4 w-4" />
-
-			<PaginationComp />
-			<Link href="/">Home</Link>
+			<DataTable columns={columns} data={employees} pagination />
 		</div>
 	);
 };
