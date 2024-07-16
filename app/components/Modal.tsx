e08d9e59aa0c79c577ff976a1f9f8e225ac72f7d
Nicolas <nicolas.deriemacker@hotmail.fr>
2024-07-16 17:42:01 +0200

    Modify modal

--- app/components/Modal.tsx
+++ app/components/Modal.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import { CircleX } from 'lucide-react';
-const Modal = () => {
+const Modal = ({ isOpen, onClose }) => {
+	const handleCloseModale = () => {
+		onClose();
+	};
 	return (
-		<div className="bg-red-500 flex flex-center items-center justify-center h-36">
-			<h1>Employee created!</h1>
-			<CircleX size={48} />
+		<div className=" fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
+			<div className="bg-primary-foreground w-1/4 h-1/7 p-4 items-center rounded-md flex justify-between">
+				<h1>Employee created!</h1>
+				<CircleX
+					size={48}
+					onClick={handleCloseModale}
+					className="cursor-pointer"
+				/>
+			</div>
 		</div>
 	);
 };
