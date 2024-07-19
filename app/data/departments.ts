15b2eced27d70044e2d746bdd013bade93de03d9
Nicolas <nicolas.deriemacker@hotmail.fr>
2024-07-19 17:20:52 +0200

    Add departments.ts

---
+++ app/data/departments.ts
@@ -0,0 +1,17 @@
+export const departments = [
+	{
+		name: 'Sales',
+	},
+	{
+		name: 'Marketting',
+	},
+	{
+		name: 'Engineering',
+	},
+	{
+		name: 'Human Resources',
+	},
+	{
+		name: 'Legal',
+	},
+];
